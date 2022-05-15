1aaa0a3935385679587ed20e55555a87d2257455
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-05-16 00:11:04 +0200

    Added placeholders

--- js.js
+++ js.js
@@ -1,3 +1,31 @@
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let date = document.querySelector("#date");
+let day = now.getDay();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+date.innerHTML = `${days[day]} ${hours}:${minutes}`;
+//
+
 let input = document.querySelector(".city");
 let form = document.querySelector("#location-form");
 let title = document.querySelector("#title");
